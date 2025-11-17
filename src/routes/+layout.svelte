30654152052238f01767ef719b49885d9fe4761e
aldottexe <aldottexe@gmail.com>
2025-11-16 19:59:33 -0500

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+{@render children()}
+
+<style lang="postcss">
+	@reference "tailwindcss";
+	:global(body) {
+		@apply text-slate-200;
+		background-image: url(https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fb%2Fbc%2FBiogradska_suma.jpg&f=1&nofb=1&ipt=9c5182c15145ea55944a21375e60771bee386da7f6e300456f4c07c1f35520ad);
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+	import Glass2 from '$lib/Glass2.svelte';
+</script>
+
+<Glass2
+	radius={30}
+	className="fixed top-5 left-5 z-5 p-4 m-10 hover:p-10 transition-all ease-[cubic-bezier(0.39,0.15,0.13,1.39)] duration-500"
+	>hello world</Glass2
+>
+
+<Glass2 radius={15} className="p-2 mx-15 mt-40">
+	<Glass2
+		radius={10}
+		className="p-2 hover:pb-15 transition-all ease-[cubic-bezier(0.39,0.15,0.13,1.39)] duration-500 mb-2"
+		>nested glass!!!</Glass2
+	>
+	<Glass2
+		radius={10}
+		className="p-2 hover:pb-15 transition-all ease-[cubic-bezier(0.39,0.15,0.13,1.39)] duration-500 mb-2"
+		>nested glass!!!</Glass2
+	>
+	<Glass2
+		radius={10}
+		className="p-2 hover:pb-15 transition-all ease-[cubic-bezier(0.39,0.15,0.13,1.39)] duration-500"
+		>nested glass!!!</Glass2
+	>
+</Glass2>
+
+<div class="max-w-100">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus asperiores, voluptatibus in
+	distinctio dignissimos optio eaque doloribus quas ipsa cupiditate expedita earum numquam accusamus
+	cumque a? Consequatur tempora cupiditate labore. Lorem ipsum dolor sit amet consectetur
+	adipisicing elit. Delectus asperiores, voluptatibus in distinctio dignissimos optio eaque
+	doloribus quas ipsa cupiditate expedita earum numquam accusamus cumque a? Consequatur tempora
+	cupiditate labore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus asperiores,
+	voluptatibus in distinctio dignissimos optio eaque doloribus quas ipsa cupiditate expedita earum
+	numquam accusamus cumque a? Consequatur tempora cupiditate labore. Lorem ipsum dolor sit amet
+	consectetur adipisicing elit. Delectus asperiores, voluptatibus in distinctio dignissimos optio
+
+	<img
+		src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fclipground.com%2Fimages%2Fcheckerboard-png-6.png&f=1&nofb=1&ipt=4db1f849a4d7e2b66daf8b1c768a379973ea2b555b4000919779d4b8154e1fbc"
+		alt="Sample"
+	/>
+	<Glass2
+		radius={10}
+		className="p-2 m-10 hover:p-10 transition-all ease-[cubic-bezier(0.39,0.15,0.13,1.39)] duration-500"
+		>this is my shader
+	</Glass2>
+	eaque doloribus quas ipsa cupiditate expedita earum numquam accusamus cumque a? Consequatur tempora
+	cupiditate labore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus asperiores, voluptatibus
+	in distinctio dignissimos optio eaque doloribus quas ipsa cupiditate expedita earum numquam accusamus
+	cumque a? Consequatur tempora cupiditate labore. Lorem ipsum dolor sit amet consectetur adipisicing
+	elit. Delectus asperiores, voluptatibus in distinctio dignissimos optio eaque doloribus quas ipsa cupiditate
+	expedita earum numquam accusamus cumque a? Consequatur tempora cupiditate labore. Lorem ipsum dolor
+	sit amet consectetur adipisicing elit. Delectus asperiores, voluptatibus in distinctio dignissimos
+	optio eaque doloribus quas ipsa cupiditate expedita earum numquam accusamus cumque a? Consequatur tempora
+	cupiditate labore.
+</div>
